4913c705e76513f258184a063e494b4303c53dac
NoahAldhous <naldhous@hotmail.co.uk>
2023-02-02 20:30:10 +0000

    added basic route handling to index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/" , ( req, res ) => {
+    res.json(myList)
+    }
+);
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
