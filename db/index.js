736c790ea7f8af871ba8747a6a8a9163627e7a0b
NoahAldhous <naldhous@hotmail.co.uk>
2023-02-07 16:24:20 +0000

    added basic connection to MongoDB cluster

--- db/index.js
+++ db/index.js
@@ -1,6 +1,3 @@
-// import express from 'express';
-// import myList from './data.js'
-
 import { MongoClient } from "mongoDB";
 
 const uri = 
@@ -21,22 +18,3 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-// const app = express();
-
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     next();
-// });
-
-// app.get("/" , ( req, res ) => {
-//     res.status(200).json(myList)
-//     }
-// );
-
-// const PORT = 3001
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`)
-// });
