44ae3cd1d7c1cc4b5a8409c9588ccccf302d5f37
NoahAldhous <naldhous@hotmail.co.uk>
2023-02-14 12:23:33 +0000

    deleted the code that was commented out in index.js as it had been moved to models.js, added a line to the getAllItems function which await a connection to mongoDB, solving a bug where the frontend was not able to connect.

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 import express from 'express';
 
-import { MongoClient } from "mongoDB";
-
 import { getAllItems } from './models/models.js';
-
-// const uri = 
-// 'mongodb+srv://Naldhous:6rF5YYN6P1pTvHEq@cluster0.b7iqhrp.mongodb.net/?retryWrites=true&w=majority'
-
-// const client = new MongoClient(uri);
-// async function getAllItems() {
-//   console.log('get request received!')
-//   try {
-//     //connect to database
-//     const database = client.db('test_todolist');
-//     const toDoList = database.collection('to_do_list');
-//     // Query for a movie that has the title 'Back to the Future'
-//     const cursor = toDoList.find({});
-//     const response = await cursor.toArray();
-//     console.log(response);
-//     return response;
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-
-//   }
-// }
 
 const app = express();
 
@@ -35,9 +11,14 @@ app.use(function(req, res, next) {
 });
 
 app.get("/" , async ( req, res ) => {
-  const items = await getAllItems().catch(console.dir);
-
-    res.status(200).json(items)
+  const list = await getAllItems().catch(console.dir);
+  
+  const responseObject = {
+    request: 'received',
+    success: 'true',
+    message: list
+  }
+    res.status(200).json(responseObject)
     }
 );
 
--- models/models.js
+++ models/models.js
@@ -4,18 +4,30 @@ import { MongoClient } from "mongoDB";
 
 const uri = 
 'mongodb+srv://Naldhous:6rF5YYN6P1pTvHEq@cluster0.b7iqhrp.mongodb.net/?retryWrites=true&w=majority'
+
 const client = new MongoClient(uri);
-//GET ALL ITEMS ON THE TO DO LIST
+
+//GET ALL ITEMS
 export async function getAllItems(){
     console.log('get request received!')
     try {
+
+      //ensures connection to MongoDB  
+      await client.connect();
+    
       //connect to database
       const database = client.db('test_todolist');
+
+      //connect to collection within database
       const toDoList = database.collection('to_do_list');
-      // Query for a movie that has the title 'Back to the Future'
+
+      //finds all {} (objects) within the collection
       const cursor = toDoList.find({});
+
+      //converts what is found to an array
       const response = await cursor.toArray();
       console.log(response);
+
       return response;
     } finally {
       // Ensures that the client will close when you finish/error
