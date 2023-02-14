31609a2092d1d41c77f0ff28749630f826dd5609
NoahAldhous <naldhous@hotmail.co.uk>
2023-02-14 11:54:44 +0000

    moved getAllItems function to model folder for separation of concerns in code

--- index.js
+++ index.js
@@ -2,28 +2,29 @@ import express from 'express';
 
 import { MongoClient } from "mongoDB";
 
-const uri = 
-'mongodb+srv://Naldhous:6rF5YYN6P1pTvHEq@cluster0.b7iqhrp.mongodb.net/?retryWrites=true&w=majority'
+import { getAllItems } from './models/models.js';
 
-const client = new MongoClient(uri);
-async function getAllItems() {
-  console.log('get request received!')
-  try {
-    //connect to database
-    const database = client.db('test_todolist');
-    const toDoList = database.collection('to_do_list');
-    // Query for a movie that has the title 'Back to the Future'
-    const cursor = toDoList.find({});
-    const response = await cursor.toArray();
-    console.log(response);
-    return response;
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+// const uri = 
+// 'mongodb+srv://Naldhous:6rF5YYN6P1pTvHEq@cluster0.b7iqhrp.mongodb.net/?retryWrites=true&w=majority'
 
-  }
-}
-getAllItems().catch(console.dir);
+// const client = new MongoClient(uri);
+// async function getAllItems() {
+//   console.log('get request received!')
+//   try {
+//     //connect to database
+//     const database = client.db('test_todolist');
+//     const toDoList = database.collection('to_do_list');
+//     // Query for a movie that has the title 'Back to the Future'
+//     const cursor = toDoList.find({});
+//     const response = await cursor.toArray();
+//     console.log(response);
+//     return response;
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+
+//   }
+// }
 
 const app = express();
 
