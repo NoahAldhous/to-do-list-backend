b17e7dc7e3d2e96989152d1a2e00d04069b604e2
NoahAldhous <naldhous@hotmail.co.uk>
2023-03-03 16:31:14 +0000

    changed import syntax in models.js so it has correct .ts file exntension on the path

--- models/models.js
+++ models/models.js
@@ -1,4 +1,4 @@
-import { ObjectId } from "mongoDB";
+import { ObjectId } from "../node_modules/mongodb/mongodb.ts";
 import { connectToDatabase, closeConnection } from '../db/index.js'
 
 //GET ALL ITEMS
