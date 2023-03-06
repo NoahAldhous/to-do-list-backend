bc999f3cc6a6775b41c94650f6309ffd4f8483fa
NoahAldhous <naldhous@hotmail.co.uk>
2023-03-06 12:41:53 +0000

    updated access-control-allow-origin to ONLY include deployed frontend URL, as only one value is allowed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 //allowing cross origin requests
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000 , https://storied-unicorn-46d331.netlify.app/");
+    res.header("Access-Control-Allow-Origin", "https://storied-unicorn-46d331.netlify.app/");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
