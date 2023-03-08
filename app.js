243b680f2b82f61001a6565afb83c2ed55699f52
NoahAldhous <naldhous@hotmail.co.uk>
2023-03-08 16:18:46 +0000

    commented out multiple origin solution as it was not working, switched back to single origin allowed, which has been set to local host

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ import listRouter from './routes/toDoListRouter.js';
 //unpacks request bodies
 app.use(express.json());
 
-const ALLOWED_ORIGINS = ["https://storied-unicorn-46d331.netlify.app", "http://localhost:3000"];
+// const ALLOWED_ORIGINS = ["https://storied-unicorn-46d331.netlify.app", "http://localhost:3000"];
 
 //allowing cross origin requests
 app.use(function(req, res, next) {
-    const origin = req.headers.origin;
-    let theOrigin = (ALLOWED_ORIGINS.indexOf(origin) >= 0) ? origin : ALLOWED_ORIGINS[0];
-    res.header("Access-Control-Allow-Origin", theOrigin);   
+    // const origin = req.headers.origin;
+    // let theOrigin = (ALLOWED_ORIGINS.indexOf(origin) >= 0) ? origin : ALLOWED_ORIGINS[0];
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");   
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
