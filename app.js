14b30197d01d54fff0f3f386531fbded7515a0ac
NoahAldhous <naldhous@hotmail.co.uk>
2023-03-08 16:09:50 +0000

    trying a different method to allow multiple request origins

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 import listRouter from './routes/toDoListRouter.js';
-import cors from 'cors';
 
 //unpacks request bodies
 app.use(express.json());
 
+const ALLOWED_ORIGINS = ["https://storied-unicorn-46d331.netlify.app", "http://localhost:3000"];
+
 //allowing cross origin requests
 app.use(function(req, res, next) {
-    console.log(req.hostname)
-    // res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
-    cors({ origin: ["http://localhost:3000", "https://storied-unicorn-46d331.netlify.app"] })
+    const origin = req.headers.origin;
+    let theOrigin = (ALLOWED_ORIGINS.indexOf(origin) >= 0) ? origin : ALLOWED_ORIGINS[0];
+    res.header("Access-Control-Allow-Origin", theOrigin);   
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
     next();
 });
 
